a6d1ed37dae6957e355dfa583b1e2b7c5b07db0f
Stewart <44583600+stewheart123@users.noreply.github.com>
2026-06-29 17:23:35 +0200

    fix: y positioning press anywhere

--- apps/card-ways/src/components/PressToContinue.svelte
+++ apps/card-ways/src/components/PressToContinue.svelte
@@ -32,7 +32,7 @@
 		height={displayHeight}
 		anchor={{ x: 0.5, y: 1 }}
 		x={context.stateLayoutDerived.mainLayout().width * 0.5}
-		y={context.stateLayoutDerived.mainLayout().height}
+		y={context.stateLayoutDerived.mainLayout().height * 0.95}
 	/>
 </MainContainer>
 <OnHotkey hotkey="Space" onpress={() => props.onpress()} />
