5b3728d534311b9348c7374475bc3afdfdd77f8d
Stewart <44583600+stewheart123@users.noreply.github.com>
2026-07-10 13:59:50 +0200

    fix: remove example loader screen

--- apps/card-ways/src/routes/+layout.svelte
+++ apps/card-ways/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import { type Snippet } from 'svelte';
 	import { base } from '$app/paths';
 	import { GlobalStyle } from 'components-ui-html';
-	import { Authenticate, LoaderStakeEngine, LoaderExample } from 'components-shared';
+	import { Authenticate, LoaderStakeEngine } from 'components-shared';
 	import { toAbsoluteAssetUrl } from 'pixi-svelte';
 	import Game from '../components/Game.svelte';
 	import InitializeLocale from '../components/InitializeLocale.svelte';
@@ -12,11 +12,8 @@
 
 	const props: Props = $props();
 
-	let showYourLoader = $state(false);
-
 	// Static files live in /static and are copied to the build root, not under _app/.
 	const loaderUrlStakeEngine = toAbsoluteAssetUrl(`${base}/stake-engine-loader.gif`);
-	const loaderUrl = toAbsoluteAssetUrl(`${base}/loader.gif`);
 
 	setContext();
 </script>
@@ -29,11 +26,6 @@
 	</Authenticate>
 </GlobalStyle>
 
-<LoaderStakeEngine src={loaderUrlStakeEngine} oncomplete={() => (showYourLoader = true)} />
-
-{#if showYourLoader}
-	<LoaderExample src={loaderUrl} />
-	<!-- Served from apps/card-ways/static/ -->
-{/if}
+<LoaderStakeEngine src={loaderUrlStakeEngine} />
 
 {@render props.children()}
